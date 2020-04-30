339a5c48a9a93edbbe62515ba47811dfb3afbad5
karthik <karthik@GCCYL3BBJ1WK.local>
2020-04-30 09:10:00 +0530

    Updates

---
+++ precache-manifest.83bd1395336b7b9958c174a94bd6cad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f624cab2fa736368cd6475679dc46e2d",
+    "url": "/karthikeof/CovidMap/index.html"
+  },
+  {
+    "revision": "33f7cd347a20ff076704",
+    "url": "/karthikeof/CovidMap/static/css/main.89c9e779.chunk.css"
+  },
+  {
+    "revision": "6b8a3af87c3348261e65",
+    "url": "/karthikeof/CovidMap/static/js/2.aa76f818.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karthikeof/CovidMap/static/js/2.aa76f818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f7cd347a20ff076704",
+    "url": "/karthikeof/CovidMap/static/js/main.8af7c46a.chunk.js"
+  },
+  {
+    "revision": "682d16dfc073380be9c4",
+    "url": "/karthikeof/CovidMap/static/js/runtime-main.cc3c1e40.js"
+  }
+]);
